5b68e19fdd8ceae11ab07184d37200a0f799195c
ElDonPapa <26351793+ElDonPapa@users.noreply.github.com>
2024-02-11 17:09:32 +0100

    Add inputToValue() function

--- rps.js
+++ rps.js
@@ -1,1 +1,11 @@
-console.log("It works!");
+"use strict";
+
+function inputToValue(pInput){
+    // Takes the input of the user and 
+    // translates it to the value of the chosen element
+    let elements = ["rock", "paper", "scissors"];
+    let input = pInput.toLowerCase();
+    return elements.indexOf(input);
+}
+
+
